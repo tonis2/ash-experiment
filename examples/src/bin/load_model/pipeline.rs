36f0c47d3964cd6a7d2b83f16d948d980dfda651
tonis2 <tonis.anton18@gmail.com>
2020-03-24 09:54:39 +0200

    Update

--- examples/src/bin/load_model/pipeline.rs
+++ examples/src/bin/load_model/pipeline.rs
@@ -313,7 +313,7 @@ impl Pipeline {
                 .expect("Unable to create graphics pipeline")
         };
 
-        let depth_image = create_depth_resources(&swapchain, &vulkan);
+        let depth_image = examples::create_depth_resources(&swapchain, &vulkan);
 
         //Destoy shader modules
         unsafe {
@@ -495,63 +495,3 @@ pub fn create_texture(image_path: &Path, vulkan: &VkInstance) -> (Image, u32) {
     (image, mip_levels)
 }
 
-//Creates depth image
-pub fn create_depth_resources(swapchain: &Swapchain, vulkan: &VkInstance) -> Image {
-    let depth_format = vulkan.find_depth_format(
-        &[
-            vk::Format::D32_SFLOAT,
-            vk::Format::D32_SFLOAT_S8_UINT,
-            vk::Format::D24_UNORM_S8_UINT,
-        ],
-        vk::ImageTiling::OPTIMAL,
-        vk::FormatFeatureFlags::DEPTH_STENCIL_ATTACHMENT,
-    );
-    let depth_image_info = vk::ImageCreateInfo {
-        s_type: vk::StructureType::IMAGE_CREATE_INFO,
-        image_type: vk::ImageType::TYPE_2D,
-        format: depth_format,
-        mip_levels: 1,
-        array_layers: 1,
-        samples: vk::SampleCountFlags::TYPE_1,
-        tiling: vk::ImageTiling::OPTIMAL,
-        usage: vk::ImageUsageFlags::DEPTH_STENCIL_ATTACHMENT,
-        sharing_mode: vk::SharingMode::EXCLUSIVE,
-        extent: vk::Extent3D {
-            width: swapchain.extent.width,
-            height: swapchain.extent.height,
-            depth: 1,
-        },
-        ..Default::default()
-    };
-
-    let mut image = Image::create_image(
-        depth_image_info,
-        vk::MemoryPropertyFlags::DEVICE_LOCAL,
-        vulkan.context(),
-    );
-
-    image.attach_view(vk::ImageViewCreateInfo {
-        s_type: vk::StructureType::IMAGE_VIEW_CREATE_INFO,
-        view_type: vk::ImageViewType::TYPE_2D,
-        format: depth_image_info.format,
-        image: image.image,
-        subresource_range: vk::ImageSubresourceRange {
-            aspect_mask: vk::ImageAspectFlags::DEPTH,
-            base_mip_level: 0,
-            level_count: 1,
-            base_array_layer: 0,
-            layer_count: 1,
-        },
-        ..Default::default()
-    });
-
-    vulkan.transition_image_layout(
-        image.image,
-        depth_format,
-        vk::ImageLayout::UNDEFINED,
-        vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
-        1,
-    );
-
-    image
-}
